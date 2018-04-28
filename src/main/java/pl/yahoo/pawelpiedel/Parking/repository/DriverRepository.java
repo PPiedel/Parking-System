c269af1111f270f4a85ca22e41d42d8a04b4da38
PPiedel <pawelpiedel@yahoo.pl>
2018-04-28 19:05:13 +0200

    Repositories annotated.

--- src/main/java/pl/yahoo/pawelpiedel/Parking/repository/DriverRepository.java
+++ src/main/java/pl/yahoo/pawelpiedel/Parking/repository/DriverRepository.java
@@ -1,7 +1,9 @@
 package pl.yahoo.pawelpiedel.Parking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.yahoo.pawelpiedel.Parking.domain.Driver;
 
+@Repository
 public interface DriverRepository extends JpaRepository<Driver,Long> {
 }
--- src/main/java/pl/yahoo/pawelpiedel/Parking/repository/Payment.java
+++ src/main/java/pl/yahoo/pawelpiedel/Parking/repository/Payment.java
@@ -1,6 +1,8 @@
 package pl.yahoo.pawelpiedel.Parking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Payment extends JpaRepository<Payment, Long> {
 }
