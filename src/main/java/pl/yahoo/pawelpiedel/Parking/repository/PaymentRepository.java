61535a42708947a87e2cad5b33970d4589462efe
PPiedel <pawelpiedel@yahoo.pl>
2018-04-28 19:15:28 +0200

    PaymentRepository name bug fixed.

--- src/main/java/pl/yahoo/pawelpiedel/Parking/repository/PaymentRepository.java
+++ src/main/java/pl/yahoo/pawelpiedel/Parking/repository/PaymentRepository.java
@@ -2,7 +2,8 @@ package pl.yahoo.pawelpiedel.Parking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.yahoo.pawelpiedel.Parking.domain.payment.Payment;
 
 @Repository
-public interface Payment extends JpaRepository<Payment, Long> {
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
