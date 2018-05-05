d892d5101928cc71784285e7174cd079d777392a
ppiedel <pawelpiedel@yahoo.pl>
2018-05-05 15:13:58 +0200

    Drivers data from .sql script removed.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,14 +8,3 @@ INSERT INTO place(id,place_status) VALUES (7,'AVAILABLE');
 INSERT INTO place(id,place_status) VALUES (8,'AVAILABLE');
 INSERT INTO place(id,place_status) VALUES (9,'AVAILABLE');
 INSERT INTO place(id,place_status) VALUES (10,'AVAILABLE');
-
-INSERT INTO driver (id, driver_type) VALUES (1, 'REGULAR');
-INSERT INTO driver (id, driver_type) VALUES (2, 'REGULAR');
-INSERT INTO driver (id, driver_type) VALUES (3, 'REGULAR');
-INSERT INTO driver (id, driver_type) VALUES (4, 'REGULAR');
-INSERT INTO driver (id, driver_type) VALUES (5, 'REGULAR');
-INSERT INTO driver (id, driver_type) VALUES (6, 'VIP');
-INSERT INTO driver (id, driver_type) VALUES (7, 'VIP');
-INSERT INTO driver (id, driver_type) VALUES (8, 'VIP');
-INSERT INTO driver (id, driver_type) VALUES (9, 'VIP');
-INSERT INTO driver (id, driver_type) VALUES (10, 'VIP');
